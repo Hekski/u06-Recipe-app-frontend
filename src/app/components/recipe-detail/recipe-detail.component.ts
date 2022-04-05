aa0bbb339a77546ce9cff1ad60e0de708f2ad82e
LesHank <henrik.berglund@gmail.com>
2022-04-05 10:56:47 +0200

    Single recipe page info and interface

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Params, Route } from '@angular/router';
-import { Recipe } from 'src/app/interface/recipe';
+import { AnalyzedInstruction, ExtendedIngredient, Recipe, } from 'src/app/interface/recipe';
 import { RecipeService } from 'src/app/services/recipe.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,25 +9,31 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-  recipes: Recipe[] = [];
-  fetchDataService: any;
+  extendedIngredients: ExtendedIngredient[] = [];
+  analyzedInstructions: AnalyzedInstruction[] = [];
+  numbers: Number[] = [];
   id!: any;
   recipe!: Recipe;
+  steps!: any;
 
   constructor(
     public recipeService: RecipeService,
-    private route: ActivatedRoute,
-    private router: Router
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     console.log('lala' + this.id);
-    
-    this.recipeService.find(this.id).subscribe((data: Recipe) => {
+
+    this.recipeService.getOneRecipe(this.id).subscribe((data: Recipe) => {
       this.recipe = data;
       console.log(data);
-      // this.recipe.summary = recipe.summary.replace(/\n/g, '<br>');
+      this.extendedIngredients = this.recipe.extendedIngredients;
+      this.analyzedInstructions = this.recipe.analyzedInstructions;
+      this.steps = this.analyzedInstructions[0].steps;
+      console.log(this.extendedIngredients);
+      console.log(this.analyzedInstructions);
+      console.log(this.steps);
     });
   }
 }
--- src/app/interface/recipe.ts
+++ src/app/interface/recipe.ts
@@ -2,19 +2,42 @@ export interface Recipe {
   id: number;
   title: string;
   image: string;
+  type: string;
   readyInMinutes: number;
   servings: number;
   vegetarian?: boolean;
+  extendedIngredients: ExtendedIngredient[];
+  analyzedInstructions: AnalyzedInstruction[];
   vegan?: boolean;
   glutenFree?: boolean;
   dairyFree?: boolean;
   sourceName: string;
-  //extendedIngredients: ExtendedIngredient[];
   summary: string;
   cuisines?: string[];
   dishTypes: string[];
   diets?: string[];
   instructions: string;
-  //analyzedInstructions: AnalyzedInstruction[];
   ingredientString: string[];
+  
+}
+
+export interface ExtendedIngredient {
+  id: number;
+  aisle?: string;
+  image: string;
+  name: string; 
+  nameClean: string; 
+  original: string; 
+  amount: number;
+  unit: string; 
+}
+
+export interface AnalyzedInstruction {
+  id: number;
+  steps: string[];
+  number: Number[];
+}
+
+export interface Number {
+  number: number;
 }
