03d5654b1334348a1f70b090c1edda1e8eab0214
LesHank <henrik.berglund@gmail.com>
2022-05-03 12:11:16 +0200

    Hiding list button unknown users

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -8,6 +8,7 @@ import { RecipeService } from 'src/app/services/recipe.service';
 import { ListService } from 'src/app/services/list.service';
 import { ActivatedRoute } from '@angular/router';
 import { List } from 'src/app/interface/list';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -25,15 +26,19 @@ export class RecipeDetailComponent implements OnInit {
   list_id: number;
   recipe_id: any;
   message: string;
+  isLoggedIn!: boolean;
 
   constructor(
     private recipeService: RecipeService,
     private listService: ListService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private loginService: LoginService
   ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
+
+    this.isLoggedIn = this.loginService.isUserLoggedIn();
 
     this.listService.getAll().subscribe((data: List[]) => {
       this.lists = Object(data);
