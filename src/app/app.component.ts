f5493bf575d3dcb67f7f6d4b5b8cf8d3a69bff97
LesHank <henrik.berglund@gmail.com>
2022-06-19 23:05:36 +0200

    Toastify logging out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LoginService } from './services/login.service';
+import { ToastService } from 'angular-toastify';
 
 
 @Component({
@@ -11,15 +12,23 @@ export class AppComponent {
   title = 'Recipe Test App';
   isLoggedIn!: boolean;
 
-  constructor(private loginService: LoginService) {
-  }
+  constructor(
+    private loginService: LoginService,
+    private _toastService: ToastService
+  ) {}
   ngOnInit() {
     this.isLoggedIn = this.loginService.isUserLoggedIn();
   }
+
+  addInfoToast() {
+    this._toastService.success('Logging out...');
+  }
+
   logout(): void {
     localStorage.removeItem('token');
     localStorage.removeItem('id');
     localStorage.removeItem('name');
-    alert("User logged out");
+    this.ngOnInit();
+    this.addInfoToast();
   }
 }
