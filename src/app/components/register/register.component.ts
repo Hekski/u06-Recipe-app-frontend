23375f775bcd0cd7888e7730585e70f16a4880fe
LesHank <henrik.berglund@gmail.com>
2022-06-20 00:33:27 +0200

    Register error feedback

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { RegisterService } from 'src/app/services/register.service';
+import { ToastService } from 'angular-toastify';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,10 +12,13 @@ import { RegisterService } from 'src/app/services/register.service';
 })
 export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
+  errorMessage: any;
+  errorStatus: any;
   constructor(
     private registerService: RegisterService,
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private _toastService: ToastService
   ) {}
   name: string = '';
   email: string = '';
@@ -37,19 +42,45 @@ export class RegisterComponent implements OnInit {
       ]),
     });
   }
-  // Registreringen måste jämföra password med confirmed password, samt skicka tillbaka följande meddelanden:
-  // - password fälten överenstämmer inte
-  // - Något fält är tomt
+
+  addInfoToast() {
+    this._toastService.error(this.errorStatus + ' ' + this.errorMessage);
+  }
 
   submitForm() {
     const formData = this.registerForm.getRawValue();
-    this.registerService.register(formData).subscribe((result: any) => {
-      result = Object(result).data;
-      localStorage.setItem('token', result.token);
-      localStorage.setItem('id', result.id);
-      localStorage.setItem('name', result.name);
-      alert(result.message);
-      this.router.navigate(['/login']);
-    });
+    this.registerService.register(formData).subscribe(
+      (result: any) => {
+        result = Object(result).data;
+        localStorage.setItem('token', result.token);
+        localStorage.setItem('id', result.id);
+        localStorage.setItem('name', result.name);
+
+        if (result) {
+          this.errorMessage = result.message;
+          this.router.navigate(['/login']).then(() => {
+            window.location.reload();
+          });
+        }
+
+        /* this.addInfoToast(result);
+      this.router.navigate(['/login']); */
+      },
+      (error) => {
+        this.errorStatus = error.status;
+        this.errorMessage =
+          (error.error.message.name ||
+          error.error.message.email ||
+          error.error.message.password ||
+          error.error.message.confirm_password); 
+        
+        console.log(error);
+        console.log(this.errorMessage);
+
+        if (!error.ok) {
+          this.addInfoToast();
+        }
+      }
+    );
   }
 }
