1b5f48cf7c09e6de9bf476c15fc9edcebd1f1070
LesHank <henrik.berglund@gmail.com>
2022-06-20 00:34:15 +0200

    Code clean up

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,7 @@ export class RegisterService {
   email: string = '';
   password: string = '';
   confirm_password: string = '';
-  LaravelApiUrl: string = 'http://u06.herokuapp.com/api/register';
+  LaravelApiUrl: string = 'https://u06.herokuapp.com/api/register';
   constructor(private http: HttpClient) {}
 
   register(formData: object): Observable<any> {
