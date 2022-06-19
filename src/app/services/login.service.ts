70b0ba3116d86ec8b5b5f6caa3b9b34f5d090040
LesHank <henrik.berglund@gmail.com>
2022-06-19 23:10:52 +0200

    code clean up

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,20 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
   email: string = '';
   password: string = '';
-  // LaravelApiUrl: string = 'http://localhost:8000/api/login';
-  LaravelApiUrl: string = 'http://u06.herokuapp.com/api/login';
+  LaravelApiUrl: string = 'https://u06.herokuapp.com/api/login';
   constructor(private http: HttpClient) {}
 
   login(formData: object): Observable<any> {
-    return this.http.post(`${this.LaravelApiUrl}`, formData);
-  }
+    return this.http.post(`${this.LaravelApiUrl}`, formData)
+ }
 
   isUserLoggedIn(): boolean {
     if (localStorage.getItem('token') != null) {
