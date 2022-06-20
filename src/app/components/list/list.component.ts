6f7b434124b62e572149f3493f541aaec6a87ab1
LesHank <henrik.berglund@gmail.com>
2022-06-20 02:31:01 +0200

    Toastify and emptymessage

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
 import { List } from 'src/app/interface/list';
 import { ListService } from 'src/app/services/list.service';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { ToastService } from 'angular-toastify';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-index',
@@ -11,36 +12,59 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ListComponent implements OnInit {
   lists: List[] = [];
+  errorStatus: any;
+  errorMessage: any;
 
   constructor(
     public listService: ListService,
     private route: ActivatedRoute,
-    public router: Router
+    public router: Router,
+    private _toastService: ToastService
   ) {}
 
   ngOnInit(): void {
     this.listService.getAll().subscribe((data: List[]) => {
       this.lists = Object(data);
-      console.log(this.lists);
     });
+  }
+
+  addInfoToast(result: string) {
+    this._toastService.info(result);
+  }
+
+  addInfoToastError() {
+    this._toastService.info(this.errorMessage);
   }
 
   createList() {
     const data = {
       title: (document.getElementById('listForm') as HTMLInputElement).value,
     };
-    this.listService.create(data).subscribe((result: any) => {
-      result = Object(result);
-      console.log(result);
-    });
-    this.ngOnInit();
+    this.listService.create(data).subscribe(
+      (result: any) => {
+        result = Object(result);
+
+        if (result) {
+          this.addInfoToast(result.message);
+          this.ngOnInit();
+        }
+      },
+      (error) => {
+        this.errorMessage = "Form is empty, name your list!";
+
+        if (!error.ok) {
+          this.addInfoToastError();
+        }
+      }
+    );
   }
 
   deleteList(id: number) {
-    this.listService.delete(id).subscribe((res) => {
+    this.listService.delete(id).subscribe((result: any) => {
+      result = Object(result);
       this.lists = this.lists.filter((item) => item.id !== id);
-      const message = 'List deleted successfully!';
-      this.ngOnInit();
+      this.addInfoToast(result.message);
+      this.router.navigate(['/list']);
     });
   }
 }
